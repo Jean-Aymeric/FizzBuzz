a2814d430e4459c2da0ee5bbc90727f368a9c653
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-09-29 19:10:49 +0200

    Commit V2

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-                            # i est un entier
-for i in range(1, 101):     # Pour i allant de 1 à 100 inclu
-    if i % 15 == 0:         #   Si i % 15 = 0 Alors
-        print("FizzBuzz")   #       Afficher("FizzBuzz")
-    elif i % 3 == 0:        #   Sinon   Si i % 3 = 0 Alors
-        print("Fizz")       #               Afficher("Fizz")
-    elif i % 5 == 0:        #           Sinon   Si i % 5 = 0 Alors
-        print("Buzz")       #                       Afficher("Buzz")
-    else:                   #                   Sinon
-        print(i)            #                       Afficher(i)
-                            #                   FinSi
-                            #           FinSi
-                            #   FinSi
-                            # FinPour
+for i in range(1, 101):
+    atLeastOneDivisor = False
+    if i % 3 == 0:
+        print("Fizz", end="")
+        atLeastOneDivisor = True
+    if i % 5 == 0:
+        print("Buzz", end="")
+        atLeastOneDivisor = True
+    if not atLeastOneDivisor:
+        print(i, end="")
+    print("")
