f459e9433921e87297c37d9f92588d1c89953d54
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-09-29 19:12:08 +0200

    change atLeastOneDivisor to atLeastOneDivider

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 for i in range(1, 101):
-    atLeastOneDivisor = False
+    atLeastOneDivider = False
     if i % 3 == 0:
         print("Fizz", end="")
-        atLeastOneDivisor = True
+        atLeastOneDivider = True
     if i % 5 == 0:
         print("Buzz", end="")
-        atLeastOneDivisor = True
-    if not atLeastOneDivisor:
+        atLeastOneDivider = True
+    if not atLeastOneDivider:
         print(i, end="")
     print("")
